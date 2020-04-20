0d4ede2bbf023c1f3d1fb4053a7cff2cb746f5e9
khvalkoilya <khvalko.ilya@gmail.com>
2020-04-21 01:33:00 +0300

    fix: make code in writing functions clearer

--- src/js/writing.js
+++ src/js/writing.js
@@ -1,29 +1,30 @@
 import vars from './variables.js';
 
-export function enterOneSymbol(symbol) {
-    vars.field.value = vars.field.value.slice(0, vars.cursor) + symbol + vars.field.value.slice(vars.cursor, vars.field.value.length);
-    vars.field.selectionEnd = vars.cursor + 1;
-  }
+function enterOneSymbol(symbol, to = 0, from = 0, number = 1, firstStop = vars.cursor, secondStop = vars.cursor) {
+    vars.field.value = vars.field.value.slice(0, firstStop+to) + symbol + vars.field.value.slice(secondStop+from, vars.field.value.length);
+    vars.field.selectionEnd = vars.cursor + number;
+}
+
+function transitionToEntering(first, second) {
+    if (vars.lang === 'ru') enterOneSymbol(first);
+    if (vars.lang === 'eng') enterOneSymbol(second);
+}
   
 export function write(item) {
     if (item.view !== 'double') {
       if (vars.pressShift || vars.pressCaps) {
-        if (vars.lang === 'ru') enterOneSymbol(item.shiftRuValue);
-        if (vars.lang === 'eng') enterOneSymbol(item.shiftEngValue);
+        transitionToEntering(item.shiftRuValue, item.shiftEngValue)
       } else {
-        if (vars.lang === 'ru') enterOneSymbol(item.shiftRuValue.toLowerCase());
-        if (vars.lang === 'eng') enterOneSymbol(item.shiftEngValue.toLowerCase());
+        transitionToEntering(item.shiftRuValue.toLowerCase(), item.shiftEngValue.toLowerCase())
       }
     } else if (item.shiftRuValue === '' && vars.lang === 'ru') {
       if (vars.pressShift || vars.pressCaps) {
         enterOneSymbol(item.ruValue);
       } else enterOneSymbol(item.ruValue.toLowerCase());
     } else if (vars.pressShift) {
-      if (vars.lang === 'ru') enterOneSymbol(item.shiftRuValue);
-      if (vars.lang === 'eng') enterOneSymbol(item.shiftEngValue);
+        transitionToEntering(item.shiftRuValue, item.shiftEngValue)
     } else {
-      if (vars.lang === 'ru') enterOneSymbol(item.ruValue);
-      if (vars.lang === 'eng') enterOneSymbol(item.engValue);
+        transitionToEntering(item.ruValue, item.engValue)
     }
   }
   
@@ -31,36 +32,42 @@ export function enterSymbols(item) {
     console.log(vars.cursor)
     window.event.preventDefault();
     if (vars.field.selectionStart === vars.field.selectionEnd) {
-      if (item.code === 'Enter') {
-        vars.field.value = `${vars.field.value.slice(0, vars.cursor)}\n${vars.field.value.slice(vars.cursor, vars.field.value.length)}`;
-        vars.field.selectionEnd = vars.cursor + 1;
-      }
-      if (item.code === 'Backspace') {
-        if (vars.cursor !== 0) {
-          vars.field.value = vars.field.value.slice(0, vars.cursor - 1) + vars.field.value.slice(vars.cursor, vars.field.value.length);
-          vars.field.selectionEnd = vars.cursor - 1;
-        }
-      }
-      if (item.code === 'Delete') {
-        vars.field.value = vars.field.value.slice(0, vars.cursor) + vars.field.value.slice(vars.cursor + 1, vars.field.value.length);
-        vars.field.selectionEnd = vars.cursor;
-      }
-      if (item.code === 'ArrowLeft') if (vars.cursor !== 0) vars.field.selectionEnd = vars.cursor - 1;
-      if (item.code === 'ArrowRight') vars.field.selectionStart = vars.cursor + 1;
-      if (item.code === 'ArrowUp') vars.field.selectionEnd = 0;
-      if (item.code === 'ArrowDown') vars.field.selectionStart = vars.field.value.length;
-      if (item.code === 'Tab') {
-        vars.field.value = `${vars.field.value.slice(0, vars.cursor)}    ${vars.field.value.slice(vars.cursor, vars.field.value.length)}`;
-        vars.field.selectionEnd = vars.cursor + 4;
+      switch (item.code){
+          case 'Enter': 
+            enterOneSymbol('\n');
+            break;
+          case 'Backspace': {
+            if (vars.cursor !== 0) {
+                enterOneSymbol('', -1, 0, -1);
+            }
+            break;
+          }   
+          case 'Delete': 
+            enterOneSymbol('',0,1,0)
+            break;
+          case 'Tab': 
+            enterOneSymbol('    ',0,0,4)
+            break;
+          case 'ArrowLeft': {
+            if (vars.cursor !== 0) 
+              vars.field.selectionEnd = vars.cursor - 1;
+            }
+            break;
+          case 'ArrowRight': 
+            vars.field.selectionStart = vars.cursor + 1;
+            break;
+          case 'ArrowUp': 
+            vars.field.selectionEnd = 0;
+            break;
+          case 'ArrowDown': 
+            vars.field.selectionStart = vars.field.value.length;
+            break;        
       }
       if (item.type === 'write') {
         write(item);
       }
-    } else if (item.code === 'Backspace' || item.code === 'Delete' || item.code === 'Tab' || item.code === 'Enter' || item.type === 'write') {
-      vars.cursor = vars.field.selectionStart;
-      vars.field.value = vars.field.value.slice(0, vars.field.selectionStart) + vars.field.value.slice(vars.field.selectionEnd, vars.field.value.length);
-      vars.field.selectionStart = vars.cursor;
-      vars.field.selectionEnd = vars.cursor;
+    } else if (item.code.match(/Backspace|Delete|Tab|Enter/) || item.type === 'write') {
+      enterOneSymbol('',0,0,0,vars.field.selectionStart,vars.field.selectionEnd)
       if (item.code !== 'Backspace' && item.code !== 'Delete') { enterSymbols(item); }
     }
   }
